472f127c929bbe1502016f3a4b6a63813cad74bc
Repan666 <alejandrorepan@gmail.com>
2025-11-16 14:55:16 +0700

    Nambah file Home sama Repan

---
+++ app/Views/home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home-Repan</title>
+</head>
+<body>
+    <h1>Welcome to Repan Home Page</h1>
+    <p>This is the home view of the Repan application.</p>
+</body>
+</html>
