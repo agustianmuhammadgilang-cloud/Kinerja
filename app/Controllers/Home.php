09e08f6536602ee0553c6a9fbd8cece69e2ca273
Repan666 <alejandrorepan@gmail.com>
2025-11-16 15:21:11 +0700

    ngubah routes

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,12 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        return view('home');
+    }
+
+    public function about(): string
+    {
+        return view('about');
     }
 }
+
